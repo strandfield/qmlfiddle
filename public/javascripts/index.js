ba353c5af1c09d356f3615349bd5379bc9d23b56
strandfield <vincent.chambrin@gmail.com>
2025-01-02 11:13:30 +0100

    generate home page with pug

---
+++ public/javascripts/index.js
@@ -0,0 +1,89 @@
+function onDragCallback() {
+    if (qtInstance) {
+        let elem = document.getElementById("screen");
+        qtInstance.qtResizeContainerElement(elem);
+        //qtInstance.qtResizeAllScreens(0);
+    }
+}
+
+window.Split(['#code', '#screen'], {
+    onDrag: onDragCallback
+});
+
+function onCurrentItemChanged() {
+    console.log("current item changed");
+}
+
+function onLintComponentIsReady() {
+  setTimeout(function() {
+    qtInstance.qmlfiddle_UseLastLintAsSource();
+  }, 16);
+}
+
+var qtInstance = null;
+var gCodeEditor = null;
+
+function SetDefaultDocument() {
+    const code = document.getElementById("code");
+    if (code.firstElementChild && code.firstElementChild.tagName == "PRE") {
+        const text = code.firstElementChild.innerHTML;
+
+        gCodeEditor.dispatch({
+            changes: {
+                from: 0,
+                to: undefined,
+                insert: text
+            }
+        });
+
+        console.log(code.firstElementChild.innerHTML);
+        code.firstElementChild.remove();
+    }
+}
+
+async function init()
+{
+    const overlay = document.querySelector('#home-overlay');
+    //const viewport = document.querySelector('#viewport');
+    const spinner = document.querySelector('#qtspinner');
+    const screen = document.querySelector('#screen');
+    const status = document.querySelector('#qtstatus');
+
+    const showUi = (ui) => {
+        [spinner, screen].forEach(element => element.style.display = 'none');
+        if (screen === ui) {
+            screen.style.position = 'default';
+            overlay.style.display = 'none';
+            SetDefaultDocument();
+        }
+        ui.style.display = 'block';
+    }
+
+    try {
+        showUi(spinner);
+        status.innerHTML = 'Loading...';
+
+        const instance = await qtLoad({
+            qt: {
+                onLoaded: () => showUi(screen),
+                onExit: exitData =>
+                {
+                    status.innerHTML = 'Application exit';
+                    status.innerHTML +=
+                        exitData.code !== undefined ? ` with code ${exitData.code}` : '';
+                    status.innerHTML +=
+                        exitData.text !== undefined ? ` (${exitData.text})` : '';
+                    showUi(spinner);
+                },
+                entryFunction: window.qmlfiddle_entry,
+                containerElements: [screen],
+                
+            }
+        });
+        
+        qtInstance = instance;
+    } catch (e) {
+        console.error(e);
+        console.error(e.stack);
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
-const path = require('path');
 
 var router = express.Router();
 
+const defaultDocument = `import QtQuick
+  
+Column {
+    anchors.centerIn: parent
+
+    spacing: 16
+
+    Image {
+        source: "qrc:/assets/qtlogo.svg"
+    }
+
+    Text {
+        anchors.horizontalCenter: parent.horizontalCenter
+        text: "❤️ You are cute! ❤️"
+        font.pointSize: 18
+    }
+}
+`;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  const filepath = path.resolve(__dirname + '/../index.html');
-  res.sendFile(filepath);
+
+  res.render('index', { 
+    title: 'QML Fiddle',
+    defaultDocument: defaultDocument 
+  });
 });
 
 module.exports = router;
