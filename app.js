db4f3bee7e9766d91dd6cded446df4fa0d354b67
strandfield <vincent.chambrin@gmail.com>
2025-01-03 17:51:34 +0100

    create fiddle database

--- app.js
+++ app.js
@@ -3,10 +3,90 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+{
+  const pjson = require('./package.json');
+  console.log(`this is ${pjson.name} version ${pjson.version}`);
+}
+
+function parseCommandLine(args = null) {
+  if (!args) {
+    args = process.argv.slice(2);
+  }
+  let result = { };
+  
+  for (let i = 0; i < args.length; ) {
+    if (args[i] == "-c" || args[i] == "--config") {
+      result.config = args[i+1];
+      i = i+2;
+    } else if(args[i] == "--data-dir") {
+      result.dataDir = args[i+1];
+      i = i+2;
+    } else {
+      console.log(`Unknown command line argument: ${args[i]}`);
+      process.exit(1);
+    }
+  }
+
+  return result;
+}
+
+let cli = parseCommandLine();
+
+let defaultDataDir = path.join(process.cwd(), 'data');
+let DataDir = defaultDataDir;
+if (process.env.QMLFIDDLE_DATA_DIR) {
+  DataDir = process.env.QMLFIDDLE_DATA_DIR;
+}
+if (cli.dataDir) {
+  DataDir = cli.dataDir;
+}
+if (DataDir != defaultDataDir) {
+  if (!fs.existsSync(DataDir)) {
+    console.log(`User-specified custom path "${DataDir}" does not exist`);
+    process.exit(1);
+  }
+} else {
+  if (!fs.existsSync(DataDir)) {
+    fs.mkdirSync(DataDir)
+  }
+}
+console.log(`Data directory is ${DataDir}`);
+
+function parseConf() {
+  let confpath = null;
+  let custom_path_conf = path.join(DataDir, "conf/app.ini");
+  if (fs.existsSync(custom_path_conf)) {
+    confpath = custom_path_conf;
+  }
+
+  if (cli.config) {
+    if (!fs.existsSync(cli.config)) {
+      console.log(`User-specified config file "${cli.config}" does not exist`);
+      process.exit(1);
+    }
+
+    confpath = cli.config;
+  }
+
+  if (confpath) {
+    console.log(`Reading config from ${confpath}`);
+    const text = fs.readFileSync(confpath, 'utf8');
+    return ini.parse(text);
+  } else {
+    return null;
+  }
+}
+
+const conf = parseConf();
+
+const { getOrCreateFiddleDatabase } = require("./src/db");
+app.locals.fiddleDatabase = getOrCreateFiddleDatabase(DataDir);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ src/db.js
@@ -0,0 +1,36 @@
+
+const Database = require('better-sqlite3');
+
+function checkTableExists(db, tableName) {
+    let stmt = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name = ?");
+    return stmt.get(tableName) != undefined;
+}
+
+function initDatabase(db) {
+    // TODO: add dateCreated, dateModified, pepper
+	db.exec(`CREATE TABLE "fiddle" (
+		"id"                    INTEGER NOT NULL PRIMARY KEY UNIQUE,
+		"title"                 TEXT NOT NULL,
+		"content"               TEXT NOT NULL
+	)`);
+}
+
+function getOrCreateFiddleDatabase(dataDir) {
+	let options = {
+        readonly: false,
+        fileMustExist: false
+      };   
+
+	const filepath = dataDir + "/fiddles.db";
+    let db = new Database(filepath, options);
+
+	if (!checkTableExists(db, "fiddle")) {
+		initDatabase(db);
+	}
+
+	return db;
+}
+
+module.exports = { 
+    getOrCreateFiddleDatabase
+};
