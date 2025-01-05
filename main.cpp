ad89846104d40ed8123cbd67101cef38d6821c7f
strandfield <vincent.chambrin@gmail.com>
2025-01-05 14:12:30 +0100

    add LICENSE

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2025 Vincent Chambrin
+// This file is part of the 'qmlfiddle' project.
+// For conditions of distribution and use, see copyright notice in LICENSE.
+
 #include <QGuiApplication>
 
 #include <QQmlEngineExtensionPlugin>
